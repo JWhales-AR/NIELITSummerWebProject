61d3ca25e1f01facb18a78d50668e5d685c0c65e
Jhonathan Whales <jwhales.rosegrid@gmail.com>
2026-07-21 23:46:54 +0530

    feat: Add a reveal on scroll animation for hero banner

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 window.addEventListener("scroll", () => {
+    headerOpacityOnScroll();
+})
+
+function headerOpacityOnScroll() {
     const header = document.getElementById("header");
 
     if (window.scrollY > 20) {
@@ -6,4 +10,20 @@ window.addEventListener("scroll", () => {
     } else {
         header.classList.remove("scrolled");
     }
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(revealOnScroll);
 })
+
+function revealOnScroll(entry) {
+    if (entry.isIntersecting) {
+        entry.target.classList.add("-revealed");
+    } else {
+        entry.target.classList.remove("-revealed");
+    }
+}
+
+document.querySelectorAll(".reveal-animated").forEach(node =>
+    observer.observe(node)
+);
