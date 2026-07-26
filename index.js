bdc095b7d61cb1a4c1cc51cd420e244fae717a6b
Jhonathan Whales <jwhales.rosegrid@gmail.com>
2026-07-26 21:43:25 +0530

    feat: Implement theme toggle

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const svgThemeToDark = `<svg class="button-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>`;
+const svgThemeToLight = `<svg class="button-icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-28 346-160H160v-186L28-480l132-134v-186h186l134-132 134 132h186v186l132 134-132 134v186H614L480-28Zm141.5-310.5Q680-397 680-480t-58.5-141.5Q563-680 480-680t-141.5 58.5Q280-563 280-480t58.5 141.5Q397-280 480-280t141.5-58.5ZM480-480Zm0 340 100-100h140v-140l100-100-100-100v-140H580L480-820 380-720H240v140L140-480l100 100v140h140l100 100Zm0-340Z"/></svg>`;
+
 const header = document.getElementById("header");
 const goToTopButton = document.getElementById("go-to-top-button");
+const themeToggleBottom = document.getElementById("header-theme-button");
 
+window.addEventListener("scroll", windowOnScroll, { passive: true });
 function windowOnScroll() {
     requestAnimationFrame(() => {
         fixHeaderAndGoToTopButton();
@@ -8,10 +13,7 @@ function windowOnScroll() {
     })
 }
 
-window.addEventListener("scroll", windowOnScroll, { passive: true });
-
 goToTopButton.addEventListener("click", () => window.scrollTo(0, 0));
-
 function fixHeaderAndGoToTopButton() {
     if (window.scrollY > 20) {
         header.classList.add("scrolled");
@@ -31,6 +33,18 @@ function progressBarUpdate() {
     let current = window.scrollY;
     let fillPercent = (current / max) * 100;
     progressBarFill.style.width = `${fillPercent}%`;
+}
+
+themeToggleBottom.addEventListener("click", toggleThemeButton);
+function toggleThemeButton() {
+    const rootElement = document.documentElement;
+    if (rootElement.classList.contains("light")) {
+        themeToggleBottom.innerHTML = svgThemeToLight;
+        rootElement.classList.remove("light");
+    } else {
+        themeToggleBottom.innerHTML = svgThemeToDark;
+        rootElement.classList.add("light");
+    }
 }
 
 const observer = new IntersectionObserver(entries => {
